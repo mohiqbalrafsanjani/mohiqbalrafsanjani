860fc9763e33bce5733871b5409d16b80cec10d8
Moh. Iqbal Rafsanjani <iqbal.outlaw@gmail.com>
2022-06-17 23:06:16 +0700

    test regex

--- readfile.py
+++ readfile.py
@@ -1,4 +1,6 @@
 import random
+import re
+
 r = random.randint(0,13)
 s = random.randint(1,26)
 f = open("list.txt","r")
@@ -10,10 +12,13 @@ def shift(text,s):
     result = ""
     for i in range(len(text)):
         char = text[i]
-        if (char.isupper()):
-            result += chr((ord(char) + s-65) % 26 + 65)
+        if (re.findall("[a-zA-Z]", char)):
+            if (char.isupper()):
+                result += chr((ord(char) + s-65) % 26 + 65)
+            else:
+                result += chr((ord(char) + s-97) % 26 +97)
         else:
-            result += chr((ord(char) + s-97) % 26 +97)
+            result += char
     return result
 
 print(shift(text,s))
