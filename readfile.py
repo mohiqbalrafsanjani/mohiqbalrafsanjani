1b8ff5250ea9d5590fd350d2ba6f21d244600f01
Moh. Iqbal Rafsanjani <iqbal.outlaw@gmail.com>
2022-06-17 22:55:29 +0700

    test cipher

--- readfile.py
+++ readfile.py
@@ -1,6 +1,19 @@
 import random
 r = random.randint(0,13)
+s = random.randint(1,26)
 f = open("list.txt","r")
 ##print(f.read())
 line = f.readlines()
-print(line[r])
+text = line[r]
+
+def shift(text,s):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        if (char.isupper()):
+            result += chr((ord(char) + s-65) % 26 + 65)
+        else:
+            result += chr((ord(char) + s-97) % 26 +97)
+        return result
+
+print(shift(text,s))
